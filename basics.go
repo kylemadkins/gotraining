8b55c9a4dea6c189a6a2c176afea019120b1bfc0
kylemadkins <kyle.m.adkins@gmail.com>
2023-07-12 16:14:24 -0400

    More example funcs

--- basics.go
+++ basics.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"regexp"
+	"strings"
 )
 
 func fizzBuzz(start int, end int) {
@@ -18,6 +21,38 @@ func fizzBuzz(start int, end int) {
 	}
 }
 
+func isEvenEnded(n int) bool {
+	str := fmt.Sprint(n)
+	return str[0] == str[len(str)-1]
+}
+
+func max(list []int) int {
+	max := list[0]
+	for _, v := range list[1:] {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+func wordCount(text string) map[string]int {
+	re, err := regexp.Compile(`[^\w]`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	text = re.ReplaceAllString(text, " ")
+	words := map[string]int{}
+	for _, word := range strings.Fields(text) {
+		words[strings.ToLower(word)] += 1
+	}
+	return words
+}
+
 func main() {
 	fizzBuzz(1, 20)
+	fmt.Println(isEvenEnded(42))
+	fmt.Println(isEvenEnded(101))
+	fmt.Println(max([]int{1, 2, 3, 9, 1}))
+	fmt.Println(wordCount("Failure to observe what is in the mind of another has seldom made a man unhappy; but those who do not observe the movements of their own minds must of necessity be unhappy."))
 }
