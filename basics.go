9c7c57a137d108f64d8315085f24d1ea2e78b54e
kylemadkins <kyle.m.adkins@gmail.com>
2023-07-12 15:22:37 -0400

    Fizz buzz example

--- basics.go
+++ basics.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 )
 
+func fizzBuzz(start int, end int) {
+	for i := start; i <= end; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("fizz buzz")
+		} else if i%3 == 0 {
+			fmt.Println("fizz")
+		} else if i%5 == 0 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
+
 func main() {
-	x, y := 1, 2
-
-	fmt.Printf("x=%v, type of %T\n", x, x)
-	fmt.Printf("y=%v, type of %T\n", y, y)
-
-	mean := float32(x + y) / 2.0
-
-	fmt.Printf("mean=%v, type of %T\n", mean, mean)
+	fizzBuzz(1, 20)
 }
